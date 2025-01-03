26c2db7d614b969baddd2cb958f1b46d066b8ce8
Jordin Gardner <jordin@dittowords.com>
2025-01-02 21:35:58 -0700

    add date string for most recent Monday

--- app/api/trmnl/route.ts
+++ app/api/trmnl/route.ts
@@ -5,12 +5,22 @@ import { getDayMarkup } from "@/app/shared/services/habit";
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
+function getMonday() {
+  const d = new Date();
+  const day = d.getDay();
+  const diff = d.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
+  return new Date(d.setDate(diff));
+}
+
 export const GET = defineAuthenticatedRoute(async (_request: NextRequest) => {
   const data = await loadHabitsWithDaysAccomplished();
   if (data) {
     const ReactDOMServer = (await import("react-dom/server")).default;
     return Response.json({
       ...data,
+      // at this point we may be introducing 3 different timezones (trmnl server, browser client,
+      // vercel server), but don't care enough to reconcile them at the moment
+      weekString: `Week of ${getMonday().toLocaleDateString()}`,
       habits: data.habits.map((habit) => {
         return {
           ...habit,
