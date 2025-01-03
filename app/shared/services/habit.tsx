24e57cf4246b7c882097909a121984efaeb9fde6
Jordin Gardner <jordin@dittowords.com>
2025-01-02 17:27:08 -0700

    darker border

--- app/shared/services/habit.tsx
+++ app/shared/services/habit.tsx
@@ -42,6 +42,7 @@ export function getDayMarkup(
         fontWeight: "bold",
         fontSize: "22px",
         color: "#757575",
+        border: "3px solid #000",
       }}
       className="bg-gray-4"
     >
